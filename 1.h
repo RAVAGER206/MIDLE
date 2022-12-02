59d56851cd7c25e1e36eb9a3c2953554353beb88
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-02 14:02:22 +0300

    Add files via upload

---
+++ 1.h
@@ -0,0 +1,12 @@
+#ifndef MIDDLE_H_INCLUDED
+#define MIDDLE_H_INCLUDED
+
+long long pere(long long i);
+
+int itc_min_num(long long a);
+
+int itc_rev_num(long long a);
+
+int itc_null_count(long long a);
+
+#endif
