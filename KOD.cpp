d3e04a1e5581cde9b446b40e5918ed4d9ccffea0
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-05 13:22:24 +0300

    Update KOD.cpp

--- KOD.cpp
+++ KOD.cpp
@@ -162,3 +162,110 @@ i = i + 1;
 }
 return b;
 }
+
+
+int itc_second_max_num(long long a){ // 12
+int d, h, g, i;
+h = 0;
+i = 0;
+if (a / 10 != 0){
+if (a < 0)
+a = a * (-1);{
+g = itc_max_num(a);
+while (a > 0){
+d = a % 10;
+if (g > d){
+if (d > h)
+h = d;
+}
+a = a / 10;
+if (d == g)
+i = i + 1;
+}
+if (i > 1)
+return g;
+return h;
+
+}
+}
+return -1;
+}
+
+
+int itc_second_simple_max_num(long long a){ // 13
+int d, h, g, i;
+h = 0;
+i = 0;
+if (a / 10 !=0){
+if (a < 0)
+a = a*(-1);
+g = itc_max_num(a);
+while (a > 0){
+d = a % 10;
+if (g > d){
+if (d > h)
+h = d;
+}
+a = a / 10;
+if (d == g)
+i = i + 1;
+}
+if (i > 1)
+return -1;
+return h;
+}
+return -1;
+}
+
+
+long long itc_bin_num(long long a){ // 14
+long long nch, d, i, g;
+nch = 0;
+i = 1;
+if (a < 0)
+a = a * (-1);
+while (a > 0){
+d = a % 2;
+nch = nch * 10 + d;
+a = a / 2;
+if (nch == 0)
+i = i * 10;
+}
+g = perevorot(nch) * i;
+return g;
+}
+
+
+long long itc_oct_num(long long a){ // 15
+long long nch, d, g, i;
+nch = 0;
+i = 1;
+if (a < 0)
+a = a * (-1);
+while (a > 0){
+d = a % 8;
+nch = nch*10 + d;
+a = a / 8;
+if (nch == 0)
+i = i * 10;
+}
+if (nch != 0){
+g = perevorot(nch) * i;
+return g;
+}
+return i;
+}
+
+
+int itc_rev_bin_num(long long a){ // 16
+int d, i, nch;
+i = 1;
+nch = 0;
+while (a > 0){
+d = a % 10;
+nch = nch + d*i;
+i = i * 2;
+a = a / 10;
+}
+return nch;
+}
