f80fd556c3b191914eff84d5a98169232df92d24
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-05 13:04:04 +0300

    Update KOD.cpp

--- KOD.cpp
+++ KOD.cpp
@@ -3,6 +3,69 @@
 
 using namespace std;
 
+void itc_num_print(long a){
+cout << a;
+}
+
+
+int itc_len_num(long long a){
+int g;
+g = 0;
+if (a == 0)
+return 1;
+if (a < 0)
+a = a * (-1);
+while (a>0){
+g = g + 1;
+a = a / 10;
+}
+return g;
+}
+
+
+int itc_sum_num(long long a){
+int g, sc;
+sc = 0;
+if (a < 0)
+a = a * (-1);
+while (a>0){
+g = a % 10;
+sc = sc + g;
+a = a / 10;
+}
+return sc;
+}
+
+
+long long itc_multi_num(long long a){
+long long g, pc;
+pc = 1;
+if (a < 0)
+a = a * (-1);
+while (a > 0){
+g = a % 10;
+pc = pc * g;
+a = a / 10;
+}
+return pc;
+}
+
+
+int itc_max_num(long long a){
+int g, h;
+g = 0;
+if (a < 0)
+a = a * (-1);
+while (a>0){
+h=a%10;
+if (h>g)
+g = h;
+a = a / 10;
+}
+return g;
+}
+
+
 long long pere(long long i){
 long long g, nch;
 nch = 0;
